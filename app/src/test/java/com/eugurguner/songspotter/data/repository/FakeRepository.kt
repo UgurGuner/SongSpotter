9b0d2e007ee45171596deaaf31f35cd732bf0e1f
UgurGuner <dadi_ugur@hotmail.com>
2023-07-12 05:05:38 +0300

    unit test for get songs use case

---
+++ app/src/test/java/com/eugurguner/songspotter/data/repository/FakeRepository.kt
@@ -0,0 +1,61 @@
+package com.eugurguner.songspotter.data.repository
+
+import com.eugurguner.songspotter.domain.model.CountEntity
+import com.eugurguner.songspotter.domain.model.Song
+import com.eugurguner.songspotter.domain.repository.SongRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class FakeRepository: SongRepository {
+
+    private val songs: ArrayList<Song> = arrayListOf()
+    private var countEntity: CountEntity = CountEntity(count = 0)
+
+    override fun getSongs(): Flow<List<Song>> {
+        return flow {
+            emit(songs)
+        }
+    }
+
+    override suspend fun getSongById(id: Int): Song? {
+        return songs.find { it.id == id }
+    }
+
+    override suspend fun insertSongs(list: List<Song>) {
+        songs.addAll(list)
+    }
+
+    override suspend fun deleteSongs() {
+        songs.clear()
+    }
+
+    override suspend fun deleteSong(song: Song) {
+        songs.remove(song)
+    }
+
+    override suspend fun getCountEntity(): CountEntity {
+        return countEntity
+    }
+
+    override suspend fun insertCountOfSongs(countEntity: CountEntity) {
+        this.countEntity = countEntity
+    }
+
+    override fun fetchSongs(
+        searchTerm: String,
+        limit: Int,
+        offset: Int,
+        onSuccess: (List<Song>, Int) -> Unit,
+        onError: (String) -> Unit
+    ) {
+        if (songs.isNotEmpty()) {
+            onSuccess(songs, countEntity.count)
+        } else {
+            onError("An error occurred")
+        }
+    }
+
+    override fun fetchLocalData(onSuccess: (List<Song>, Int) -> Unit) {
+        onSuccess(songs, countEntity.count)
+    }
+}
---
+++ app/src/test/java/com/eugurguner/songspotter/domain/use_case/GetSongsUseCaseTest.kt
@@ -0,0 +1,83 @@
+package com.eugurguner.songspotter.domain.use_case
+
+import com.eugurguner.songspotter.data.repository.FakeRepository
+import com.eugurguner.songspotter.domain.model.Song
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class GetSongsUseCaseTest {
+
+    private lateinit var fakeRepository: FakeRepository
+    private lateinit var getSongsUseCase: GetSongsUseCase
+
+    @Before
+    fun setUp() {
+
+        fakeRepository = FakeRepository()
+        getSongsUseCase = GetSongsUseCase(fakeRepository)
+
+        val songsToInsert = mutableListOf<Song>()
+        ('a'..'z').forEachIndexed { index, c ->
+            songsToInsert.add(
+                Song(
+                    wrapperType = c.toString(),
+                    kind = c.toString(),
+                    collectionId = index,
+                    trackId = index,
+                    artistName = c.toString(),
+                    collectionName = c.toString(),
+                    trackName = c.toString(),
+                    collectionCensoredName = c.toString(),
+                    trackCensoredName = c.toString(),
+                    collectionArtistId = c.toString(),
+                    collectionArtistViewUrl = c.toString(),
+                    collectionViewUrl = c.toString(),
+                    trackViewUrl = c.toString(),
+                    previewUrl = c.toString(),
+                    artworkUrl30 = c.toString(),
+                    artworkUrl60 = c.toString(),
+                    artworkUrl100 = c.toString(),
+                    collectionPrice = 0.0,
+                    trackPrice = 0.0,
+                    trackRentalPrice = 0.0,
+                    collectionHdPrice = 0.0,
+                    trackHdPrice = 0.0,
+                    trackHdRentalPrice = 0.0,
+                    releaseDate = c.toString(),
+                    collectionExplicitness = c.toString(),
+                    trackExplicitness = c.toString(),
+                    discCount = index,
+                    discNumber = index,
+                    trackCount = index,
+                    trackNumber = index,
+                    trackTimeMillis = index,
+                    country = c.toString(),
+                    currency = c.toString(),
+                    primaryGenreName = c.toString(),
+                    contentAdvisoryRating = c.toString(),
+                    shortDescription = c.toString(),
+                    longDescription = c.toString(),
+                    hasITunesExtras = false
+                )
+            )
+        }
+        songsToInsert.shuffle()
+        runBlocking {
+            fakeRepository.insertSongs(songsToInsert)
+        }
+
+    }
+
+    @Test
+    fun `Songs If Inserted`() = runBlocking {
+
+        val songs = getSongsUseCase.invoke().first()
+
+        assertThat(songs.isNotEmpty()).isTrue()
+
+    }
+
+}
